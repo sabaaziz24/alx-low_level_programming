f6f61aa531f73569e489680f5e25c19d9e655e71
sabaaziz24 <sabaabdulbasit24@gmail.com>
2023-09-11 10:08:46 -0700

    m

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	int i, j, k;
-	
+
 	for (i = '0'; i <= '9'; i++)
 	{
 		for (j = i + 1; j <= '9'; j++)
@@ -18,7 +18,7 @@ int main(void)
 				putchar(i);
 				putchar(j);
 				putchar(k);
-				
+
 				if (i != '7')
 					{
 						putchar(',');
