d617287377a3564bee0e67c6fde68e463914521c
sabaaziz24 <sabaabdulbasit24@gmail.com>
2023-09-11 09:34:37 -0700

    m

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,16 +11,20 @@ int main(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-	    for (i = c + 1; i <= '9'; i++)
-	    {
-	        putchar(c);
-	        putchar(i);
-	        if (c != '8' || (c == '8' && i != '9'))
+		for (i = '0'; i <= '9'; i++)
 		{
-	  	    putchar(',');
-		    putchar(' ');
+			if (c < i)
+			{
+				putchar(c);
+				putchar(i);
+
+				if (c != '8' || (c == '8' && i != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
-	    }
 	}
 
 	putchar('\n');
