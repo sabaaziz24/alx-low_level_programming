482fb4361679daf5ac35313eb34a33003b0b15ea
sabaaziz24 <sabaabdulbasit24@gmail.com>
2023-09-11 09:14:47 -0700

    m

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+  * main - Prints combination of numbers
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	int c, i;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (i = c+1; i <= '9'; i++)
+		{	
+		       	putchar(c);
+       			putchar(i);
+
+       			if (c != '8' || (c == '8' && i != '9'))
+		       	{
+	       			putchar(',');
+       				putchar(' ');
+		        }
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
