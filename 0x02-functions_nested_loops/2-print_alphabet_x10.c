ea1c798f7b0f4f466b7354eebb82d6a5a4bfbf7d
sabaaziz24 <sabaabdulbasit24@gmail.com>
2023-09-12 23:35:35 -0700

    m

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_alphabet - xx
+*
+* return: coid
+*/
+void print_alphabet_x10(void)
+{
+        char a;
+        for (int i = 0; i < 10; i++)
+        {
+                /* code */
+                for (a = 'a'; a <= 'z'; a++)
+                {
+                        _putchar(a);
+                }
+
+                _putchar('\n');
+
+        }
+}
