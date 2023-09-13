d5887844219cf4ae42f75b7976843e0a1f854053
sabaaziz24 <sabaabdulbasit24@gmail.com>
2023-09-13 01:06:16 -0700

    m

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+* main - xx
+*
+* return: coid
+*/
+int main(void)
+{
+	int i, ans;
+	for (i = 1; i < 1024; i++)
+	{
+		if (i%3 == 0)
+		{
+			ans += i
+		}
+
+		else if (i % 5 == 0)
+		{
+			ans += i
+		}
+	}
+
+	printf("%d\n", ans);
+}
