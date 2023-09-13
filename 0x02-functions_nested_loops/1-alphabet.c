983a1417dfaedca3b13daff92b4a57475f934f09
sabaaziz24 <sabaabdulbasit24@gmail.com>
2023-09-12 23:20:24 -0700

    m

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**                                                                                                                                                                     
+ * main -  prints _putchar, followed by a new line                                                                                                                      
+ * Return: Always 0 (Success)                                                                                                                                           
+ */
+void print_alphabet(void){
+        char a;
+        for (a = 'a'; a <= 'z'; a++){
+                _putchar(a)
+        }
+}
