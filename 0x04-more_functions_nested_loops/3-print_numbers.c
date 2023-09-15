5d54a580dc6e0e38c1c11e2655b2eadc62be4e0e
sabaaziz24 <sabaabdulbasit24@gmail.com>
2023-09-15 01:10:40 -0700

    m

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* int mul - xx
+*
+* Return: coidss
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+
+	return;
+}
