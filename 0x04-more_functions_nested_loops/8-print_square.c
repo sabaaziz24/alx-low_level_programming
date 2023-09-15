2c6b2d1165de100dc68ae77f81a5efe532d5477c
sabaaziz24 <sabaabdulbasit24@gmail.com>
2023-09-15 01:53:36 -0700

    m

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_square - xx
+* @size: ewf
+* Return: coidss
+*/
+void print_square(int size)
+{
+	int i, j;
+
+	for (j = 0; j < size; j++)
+	{
+		for (i = 0; i < size; i++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
