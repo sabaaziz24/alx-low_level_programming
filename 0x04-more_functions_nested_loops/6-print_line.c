459f345dc401a6372ecc472012a572f4cb86b5ee
sabaaziz24 <sabaabdulbasit24@gmail.com>
2023-09-15 01:35:11 -0700

    m

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_line - xx
+*
+* Return: coidss
+*/
+void print_line(int n)
+{
+	int j;
+
+	for (j = 0; j < n; j++)
+	{
+		_putchar('_');
+	}
+
+	_putchar('\n');
+}
