e04dfce586f63ee189fe6356226baf846df48521
sabaaziz24 <sabaabdulbasit24@gmail.com>
2023-09-15 00:36:59 -0700

    m

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+* int _isupper - xx
+*
+* Return: coid
+*/
+int _isupper(int c)
+{
+	if (c >='a' && c <= 'z')
+	{
+		return (1);
+	}
+
+	return (0);
+}
