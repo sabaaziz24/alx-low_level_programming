9ac6ba0d6cd3fc057dd10b893236cdd60c261f3f
sabaaziz24 <sabaabdulbasit24@gmail.com>
2023-09-15 01:29:17 -0700

    m

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* more_numbers - xx
+*
+* Return: coidss
+*/
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+
+	_putchar('\n');
+}xs
