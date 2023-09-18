36542673398c1c09a6f272d75f1a1d2ab42e31b1
sabaaziz24 <sabaabdulbasit24@gmail.com>
2023-09-18 06:55:33 -0700

    m

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _atoi - aa
+ * @s: ss
+ * Return: wahr
+ */
+int _atoi(char *s)
+{
+	int i = 0, n = 0, fir = 0, ans = 0, tmp;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			n++;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			tmp = s[i] - '0';
+			ans = 10 * ans + tmp;
+			fir = 1;
+		}
+		else if (!((s[i] == '+' || s[i] == ' ') && fir == 0))
+			break;
+
+	}
+
+	return (ans);
+}
