165093b75f719900b1c798d69e12fb1f13dbe835
sabaaziz24 <sabaabdulbasit24@gmail.com>
2023-09-18 06:01:01 -0700

    m

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+* rev_string - xx
+* @s: sjb
+* Return: coidss
+*/
+void rev_string(char *s)
+{
+	int i = 0, len;
+	char *s2;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	len = i;
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		s2[len - i - 1] = s[i];
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = s2[i];
+	}
+}
