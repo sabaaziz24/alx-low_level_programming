eee56f0d26708c0d2da56346d4d1676380162433
sabaaziz24 <sabaabdulbasit24@gmail.com>
2023-09-18 07:36:49 -0700

    m

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _atoi - aa
+ * @s: ss
+ * Return: wahr
+ */
+int _atoi(char *s)
+{
+	int i = 0, n = 0, fir = 0, tmp;
+	unsigned int ans = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			n++;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			tmp = s[i] - '0';
+			ans = 10 * ans + tmp;
+			fir = 1;
+		}
+		else if (fir)
+			break;
+
+		i++;
+
+	}
+	if (n % 2)
+		return (-ans);
+
+	return (ans);
+}
