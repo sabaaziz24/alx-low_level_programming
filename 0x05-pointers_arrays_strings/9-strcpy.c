5309a1c42708a43013c356d4b2eacbc2d7b8f5fb
sabaaziz24 <sabaabdulbasit24@gmail.com>
2023-09-18 06:28:12 -0700

    m

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_strcpy - aa
+ * @dest: aa
+ * @src: saa
+ *
+ * Return: wahr
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
+
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
