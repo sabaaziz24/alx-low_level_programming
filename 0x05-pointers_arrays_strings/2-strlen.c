72478823b5b37af5fbaddcf5d06969b6e077c92f
sabaaziz24 <sabaabdulbasit24@gmail.com>
2023-09-18 05:41:42 -0700

    m

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+* _strlen - xx
+* @s: sjb
+* Return: coidss
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
