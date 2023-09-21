8687ef42add5cdb455b65fafeb999a330239d0f5
sabaaziz24 <sabaabdulbasit24@gmail.com>
2023-09-21 02:57:13 -0700

    m

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncat - so
+ * @dest: x
+ * @src: so
+ * @n: x
+ *
+ * Return: xz
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
