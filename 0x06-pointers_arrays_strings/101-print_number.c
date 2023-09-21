dc9634590fe3a18bcb17abe143ad7a6913159a7a
sabaaziz24 <sabaabdulbasit24@gmail.com>
2023-09-21 03:37:41 -0700

    m

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,14 +8,19 @@
  */
 void print_number(int n)
 {
+	unsigned int n1;
+
 	if (n < 0)
 	{
-		n = -n;
+		n1 = -n;
 		_putchar('-');
+	} else
+	{
+		n1 = n;
 	}
 
-	if (n / 10)
-		print_number(n / 10);
+	if (n1 / 10)
+		print_number(n1 / 10);
 
-	_putchar((n % 10) + '0');
+	_putchar((n1 % 10) + '0');
 }
