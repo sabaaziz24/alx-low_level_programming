8a3953ed6e7fe86fe64ffe6c362f35d3379cf445
sabaaziz24 <sabaabdulbasit24@gmail.com>
2023-09-21 02:39:47 -0700

    m

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * *_strcat - aa
+ * @*dest: ss
+ * @*src: zz
+ * Return: wahr
+ */
+char *_strcat(char *dest, char *src)
+{
+	int n1 = 0, n2 = 0, i;
+
+	while (src[n1] != '\0')
+		n1++;
+
+	while (dest[n2] != '\0')
+		n2++;
+
+	for (i = n2; i < n2 + n1; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i] = '\0'
+	return (*dest);
+}
