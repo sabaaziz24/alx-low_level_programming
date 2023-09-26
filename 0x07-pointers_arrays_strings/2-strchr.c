339b1827976da4b1e1b9d1fc526e2341efa08ef2
sabaaziz24 <sabaabdulbasit24@gmail.com>
2023-09-26 07:36:57 -0700

    m

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * _strchr - so
- * @c: df
- * @s: v
+ * _strchr - aa
+ * @s: aa
+ * @c: aa
  *
- * Return: so
+ * Return: aa
  */
 char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
-			return (s+i);
+			return (s + i);
 	}
 
 	return ('\0');
