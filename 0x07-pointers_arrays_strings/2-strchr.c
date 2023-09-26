81a53e0957bfffb4674872b3331d9486b1dd8a1d
sabaaziz24 <sabaabdulbasit24@gmail.com>
2023-09-26 07:28:08 -0700

    m

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - so
+ * @c: df
+ * @s: v
+ *
+ * Return: so
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i=0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s+i)
+
+	}
+
+	return ('\0');
+}
