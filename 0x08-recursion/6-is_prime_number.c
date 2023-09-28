2860cdf152be39c1ab57aa07005e851551abc20a
sabaaziz24 <sabaabdulbasit24@gmail.com>
2023-09-28 02:00:41 -0700

    hi

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * is_divisible - cc
+ * @n: cc
+ * @div: cc
+ *
+ * Return: result
+ */
+int is_divisible(int n, int div)
+{
+	if (n % div == 0)
+	{
+		return (0);
+	}
+
+	if (div == n / 2)
+	{
+		return (1);
+	}
+
+	return (is_divisible(n, div + 1));
+}
+
+/**
+ * is_prime_number - cc
+ * @n: cc
+ *
+ * Return: result
+ */
+int is_prime_number(int n)
+{
+	int div = 2;
+
+	if (n <= 1)
+	{
+		return (0);
+	}
+
+	if (n <= 3)
+	{
+		return (1);
+	}
+
+	return (is_divisible(n, div));
+}
