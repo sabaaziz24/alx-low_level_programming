220a138e700642bc6751631fce2043f20b8bc506
sabaaziz24 <sabaabdulbasit24@gmail.com>
2023-09-28 01:13:54 -0700

    hi

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * factorial - xx
+ * @n: xx
+ *
+ * Return: string length
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
