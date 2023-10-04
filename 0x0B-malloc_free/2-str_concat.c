05cb3c41f148f6fbad966509b74a0e4a391e0a2e
sabaaziz24 <sabaabdulbasit24@gmail.com>
2023-10-04 08:47:07 -0700

    m

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+/**
+ * str_concat - cc
+ * @s1: cc
+ * @s2: cc
+ *
+ * Return: result
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *p;
+	unsigned int i, n1 = 0, n2 = 0;
+
+	while (s1[n1] != '\0')
+		n1++;
+
+	while (s2[n2] != '\0')
+		n2++;
+	n2++;
+
+	p = malloc((n1 + n2) * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < (n1 + n2); i++)
+	{
+		if (i < n1)
+			*(p + i) = *(s1 + i);
+		else
+			*(p + i) = *(s2 + i - n1);
+	}
+
+	return (p);
+}
