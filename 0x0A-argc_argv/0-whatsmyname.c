7edbb3818db9aeb174d4055e99f80c74e7a25494
sabaaziz24 <sabaabdulbasit24@gmail.com>
2023-10-06 00:41:36 -0700

    m

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+  * main - cc
+  *@argc: cc
+  *@argv: cc
+  *
+  *Return: cc
+  */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return 0;
+}
