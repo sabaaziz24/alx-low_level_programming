d643fa9f6b45cdd641fe57dd7a788c15888ef2c0
sabaaziz24 <sabaabdulbasit24@gmail.com>
2023-10-06 01:07:23 -0700

    m

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,11 +9,16 @@
  */
 int main(int argc, char *argv[])
 {
+	int num1, num2;
+
 	if (argc != 3)
 	{
 		printf("%s\n", "Error");
 		return (1);
 	}
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	printf("%d\n", num1 * num2);
 	return (0);
 }
