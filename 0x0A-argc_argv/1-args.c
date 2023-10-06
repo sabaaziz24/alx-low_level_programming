d4063e6c95506649a1236714933f1b38e31b6883
sabaaziz24 <sabaabdulbasit24@gmail.com>
2023-10-06 00:50:55 -0700

    m

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,7 +7,7 @@
  * @argv: an array of a command listed
  * Return: 0 for success
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
