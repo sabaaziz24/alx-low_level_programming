13152e962e1636dabc9aad56204e972cd1a70c01
sabaaziz24 <sabaabdulbasit24@gmail.com>
2023-10-09 09:21:23 -0700

    m

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,28 +9,36 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int n1 = 0, n2 = 0, i = 0;
-	char *p;
+	unsigned int n1 = 0, n2 = 0, i;
+	char *s;
 
+	/* if NULL is passed, treat as an empty string */
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	/* While at index 0 */
 	while (s1[n1] != '\0')
-	{
 		n1++;
-	}
-	while (s2[n2] != '\0' && n2 <= n)
-	{
+	while (s2[n2] != '\0')
 		n2++;
-	}
 
-	p = malloc(n1+n2+1);
-	if (p == NULL)
+	if (n >= n2)
+		n = n2;
+
+	/* Memory allocation */
+	s = malloc(sizeof(char) * n + n1 + 1);
+	if (s == NULL)
 		return (NULL);
-	for (i = 0; i < n1+n2; i++)
-	{
-		if (i < n1)
-			p[i] = s1[i];
-		else
-			p[i] = s2[i - n1];
-	}
-	p[n1 + n2] = '\0';
-	return (p);
+
+	for (i = 0; i < n1; i++)
+		s[i] = s1[i];
+
+	for (i = 0; i < n; i++)
+		s[i + n1] = s2[i];
+
+	s[i + n1] = '\0';
+
+	return (s);
 }
