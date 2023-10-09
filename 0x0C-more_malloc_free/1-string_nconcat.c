dbb4cab92d310e9016ddcddd10b2a58b1c52bc88
sabaaziz24 <sabaabdulbasit24@gmail.com>
2023-10-09 09:12:05 -0700

    m

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * string_nconcat - al
+ * @s1: n
+ * @s2: n
+ * @n: n
+ * Return: p
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int n1 = 0, n2 = 0, i = 0;
+	char *p;
+
+	while (s1[n1] != '\0')
+	{
+		n1++;
+	}
+	while (s2[n2] != '\0' && n2 <= n)
+	{
+		n2++;
+	}
+
+	p = malloc(n1+n2+1);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < n1+n2; i++)
+	{
+		if (i < n1)
+			p[i] = s1[i];
+		else
+			p[i] = s2[i - n1];
+	}
+	p[n1 + n2] = '\0';
+	return (p);
+
+	return (str);
+}
